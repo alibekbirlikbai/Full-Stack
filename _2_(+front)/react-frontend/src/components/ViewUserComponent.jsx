7c765ae5ce0f283a65ceb7b89c8c945428177678
Alibek Birlikbai <alibekbirlikbai21@gmail.com>
2022-09-30 02:03:24 +0600

    initial commit

---
+++ _2_(+front)/react-frontend/src/components/ViewUserComponent.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import UserService from '../services/UserService'
+
+class ViewUserComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            user: {}
+        }
+    }
+
+    componentDidMount(){
+        UserService.getUserById(this.state.id).then( res => {
+            this.setState({user: res.data});
+        })
+    }
+
+    toList(){
+        this.props.history.push('/users');
+    }
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                <div className = "card col-md-6 offset-md-3">
+                    <h3 className = "text-center"> View User Details</h3>
+                    <div className = "card-body">
+                        <div className = "row">
+                            <label> First Name: </label>
+                            <div> { this.state.user.firstName }</div>
+                        </div>
+                        <div className = "row">
+                            <label> Last Name: </label>
+                            <div> { this.state.user.lastName }</div>
+                        </div>
+                        <div className = "row">
+                            <label> Email ID: </label>
+                            <div> { this.state.user.emailId }</div>
+                        </div>
+
+                        <button className="btn btn-success" onClick={this.toList.bind(this)}>Back</button>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ViewUserComponent
